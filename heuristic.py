f5f3c7ca52b0f5362e1e367640d0b06c013d5d90
Anubhav <anubhav.14apr@gmail.com>
2022-07-23 14:01:36 +0530

    functions defined

---
+++ heuristic.py
@@ -0,0 +1,20 @@
+def create_demands():
+    pass
+
+def create_virtual_links():
+    pass
+
+def preprocess():
+    pass
+
+def find_path():
+    pass
+
+def replenish():
+    """
+    Replenish VL after at every timestep
+    """
+    pass
+
+def main():
+    
